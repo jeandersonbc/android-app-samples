4f3f28433f277be8447d210b97ae631d3489d351
Jeanderson Candido <jeandersonbc@gmail.com>
2018-05-23 16:25:14 -0300

    AppBarDemo - Added menu to the app bar and onOptionsItemSelected callback

--- kotlin/AppBarDemo/app/src/main/java/com/example/jeandersonbc/appbardemo/MainActivity.kt
+++ kotlin/AppBarDemo/app/src/main/java/com/example/jeandersonbc/appbardemo/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.jeandersonbc.appbardemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,4 +13,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    // Have some time to appreciate this construction:
+    override fun onOptionsItemSelected(item: MenuItem) =
+            when (item.itemId) {
+                R.id.action_someAction1 -> {
+                    Toast.makeText(this, "Action 1 Triggered!", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.action_someAction2 -> {
+                    Toast.makeText(this, "Action 2 Triggered!", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    super.onOptionsItemSelected(item)
+                }
+            }
 }
