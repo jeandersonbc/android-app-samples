c90e4907174c15e63f282aa574a9ca508e25c5f8
Jeanderson Candido <jeandersonbc@gmail.com>
2018-06-12 17:32:12 -0300

    ImplicitIntentsDemo - Added callback to open Maps location

--- java/ImplicitIntentsDemo/app/src/main/java/com/example/jeandersonbc/implicitintentsdemo/MainActivity.java
+++ java/ImplicitIntentsDemo/app/src/main/java/com/example/jeandersonbc/implicitintentsdemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
+    }
+
+    public void openSchoolLocation(View view) {
+        String addressString = "Av. Jorn. Aníbal Fernandes, s/n - Cidade Universitária, Recife - PE, 50740-560";
+        Uri.Builder builder = new Uri.Builder();
+        Uri geoUri = builder.scheme("geo").path("0,0").query(addressString).build();
+
+        // TODO: Open location
+        Toast.makeText(this, String.format("%s", geoUri), Toast.LENGTH_SHORT).show();
+
     }
 
 }
