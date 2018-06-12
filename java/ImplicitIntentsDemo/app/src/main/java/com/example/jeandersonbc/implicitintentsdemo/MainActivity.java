5afaae55c70833d485f808b6fa068b5371d5528a
Jeanderson Candido <jeandersonbc@gmail.com>
2018-06-12 17:40:40 -0300

    ImplicitIntentsDemo - WIP/Opening map in a desired location

--- java/ImplicitIntentsDemo/app/src/main/java/com/example/jeandersonbc/implicitintentsdemo/MainActivity.java
+++ java/ImplicitIntentsDemo/app/src/main/java/com/example/jeandersonbc/implicitintentsdemo/MainActivity.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,12 +25,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openSchoolLocation(View view) {
-        String addressString = "Av. Jorn. Aníbal Fernandes, s/n - Cidade Universitária, Recife - PE, 50740-560";
+        // FIXME not display the proper place
+        String addressString = "47.6,-122.3(My place)";
         Uri.Builder builder = new Uri.Builder();
         Uri geoUri = builder.scheme("geo").path("0,0").query(addressString).build();
 
-        // TODO: Open location
-        Toast.makeText(this, String.format("%s", geoUri), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
 
     }
 
